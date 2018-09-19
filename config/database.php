4e16e1e8907f12bc91582e22ab2d5f0a06679ffe
Vladislav Gushchin <vladislav.g@cronix.ms>
2018-09-19 16:14:50 +0300

    fa

--- config/database.php
+++ config/database.php
@@ -1,7 +1,36 @@
 <?php
 
 return [
+    $cleardb_url      = parse_url(getenv("mysql://bb0d02f5291e7b:08e0da38@us-cdbr-iron-east-01.cleardb.net/heroku_97b8078c6c2dcde?reconnect=true"));
+$cleardb_server   = $cleardb_url["us-cdbr-iron-east-01.cleardb.net"];
+$cleardb_username = $cleardb_url["bb0d02f5291e7b"];
+$cleardb_password = $cleardb_url["08e0da38"];
+$cleardb_db       = substr($cleardb_url["heroku_97b8078c6c2dcde"],1);
 
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+    'dsn'   => '',
+    'hostname' => $cleardb_server,
+    'username' => $cleardb_username,
+    'password' => $cleardb_password,
+    'database' => $cleardb_db,
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
--- server.php
+++ server.php
@@ -10,39 +10,6 @@
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
-
-$cleardb_url      = parse_url(getenv("mysql://bb0d02f5291e7b:08e0da38@us-cdbr-iron-east-01.cleardb.net/heroku_97b8078c6c2dcde?reconnect=true"));
-$cleardb_server   = $cleardb_url["us-cdbr-iron-east-01.cleardb.net"];
-$cleardb_username = $cleardb_url["bb0d02f5291e7b"];
-$cleardb_password = $cleardb_url["08e0da38"];
-$cleardb_db       = substr($cleardb_url["heroku_97b8078c6c2dcde"],1);
-
-$active_group = 'default';
-$query_builder = TRUE;
-
-$db['default'] = array(
-    'dsn'   => '',
-    'hostname' => $cleardb_server,
-    'username' => $cleardb_username,
-    'password' => $cleardb_password,
-    'database' => $cleardb_db,
-    'dbdriver' => 'mysqli',
-    'dbprefix' => '',
-    'pconnect' => FALSE,
-    'db_debug' => (ENVIRONMENT !== 'production'),
-    'cache_on' => FALSE,
-    'cachedir' => '',
-    'char_set' => 'utf8',
-    'dbcollat' => 'utf8_general_ci',
-    'swap_pre' => '',
-    'encrypt' => FALSE,
-    'compress' => FALSE,
-    'stricton' => FALSE,
-    'failover' => array(),
-    'save_queries' => TRUE
-);
-
-
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
